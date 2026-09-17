e18da9bf36f7903b237381aa95ac5ba4b299ecb5
Val <60014136+voffee@users.noreply.github.com>
2026-09-18 05:02:05 +0800

    scrolling for tab panels partial fix

--- js/app.js
+++ js/app.js
@@ -12,21 +12,26 @@ const modal = document.querySelector(".modal");
 function tabClickHandler(e) {
     tabs.forEach(element => {
         element.ariaSelected = false;
+        // tabs are the tablist button, i.e. tabs that control their associated tab panel
+        // set all tabs to hidden on click
     });
 
     e.target.ariaSelected = true;
+    // enable clicked tab panel
 
     const tabID = e.target.getAttribute('aria-controls');
+    console.log(tabID);
+    //find the tab panel that the clicked tab controls 
 
     tabPanels.forEach(element => {
-        // const elementStyle = window.getComputedStyle(element);
-
+        // loop through all the tab panels to find which is the one controlled by the clicked tab
         if(element.id === tabID) {
             element.hidden = false;
-            element.style.overflowY = 'auto';
+            // set the controlled panel to display
         }
         else {
             element.hidden = true;
+            // set all other panels to hidden
         }
     });
 }
